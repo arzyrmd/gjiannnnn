66b2bce52b47d4df2846ec08d38b36b57e5c0dfa
arzyrmd <ategal505@gmail.com>
2026-09-03 00:22:42 +0700

    fix: set fallback APP_KEY and smart exception renderer for Vercel

--- config/app.php
+++ config/app.php
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:nIETmmyRblG5BQ2BRzwFSvkt3STBSxh6/D1bH2ovjTs='),
 
     'previous_keys' => [
         ...array_filter(
